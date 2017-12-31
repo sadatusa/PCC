e15f12cac1d0f7a8d0b9f358b024a3052941a1bd
sadatusa <sadatusa@gmail.com>
2017-12-31 14:29:47 +0500

    Python Crash Course

---
+++ 6_1.py
@@ -0,0 +1,10 @@
+# 6-1. Person: Use a dictionary to store information about a person you know.
+# Store their first name, last name, age, and the city in which they live. You
+# should have keys such as first_name, last_name, age, and city. Print each
+# piece of information stored in your dictionary.
+
+Person_info={'first_name':'Sadat','last_name':'Hussain', 'age':'40','city':'Karachi'}
+print('First Name : '+Person_info['first_name'])
+print('Last Name  : '+Person_info['last_name'])
+print('Age        : '+Person_info['age'])
+print('City       : '+Person_info['city'])
---
+++ 6_2.py
@@ -0,0 +1,11 @@
+# 6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers.
+# Think of five names, and use them as keys in your dictionary. Think of a favorite
+# number for each person, and store each as a value in your dictionary. Print
+# each person’s name and their favorite number. For even more fun, poll a few
+# friends and get some actual data for your program.
+
+Favorite_Numbers={'sadat':2,'areeb':4,'naeem':7,'aslam':1}
+print('SADAT   : '+str(Favorite_Numbers['sadat']))
+print('AREEB   : '+str(Favorite_Numbers['areeb']))
+print('NAEEM   : '+str(Favorite_Numbers['naeem']))
+print('ASLAM   : '+str(Favorite_Numbers['aslam']))
